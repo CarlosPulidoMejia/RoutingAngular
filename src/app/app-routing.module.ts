b77a10286767efdb85dd88b20a523fcdea5ce60e
CarlosPulidoMejia <cjlpulido@hotmail.com>
2023-05-23 17:49:38 -0600

    guards y navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
+import { ContactDetailPageComponent } from './pages/contact-detail-page/contact-detail-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { authGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+    children: [
+      {
+        path: 'hijo',
+        component: HomePageComponent
+      }
+    ]
+  },
+  {
+    path:'contacts',
+    component: ContactsPageComponent,
+    canActivate: [ authGuard ]
+
+  },
+  {
+    path: 'contacts/:id',
+    component: ContactDetailPageComponent,
+    canActivate: [ authGuard ]
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/contacts-page/contacts-page.component.ts
+++ src/app/pages/contacts-page/contacts-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IContacto } from 'src/app/models/contact.interface';
 
 @Component({
   selector: 'app-contacts-page',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacts-page.component.scss']
 })
 export class ContactsPageComponent {
+  
+  listaContactos: IContacto[] = [
+    {
+      id:0,
+      nombre: 'Carlos',
+      apellidos: 'Pulido Mejia',
+      email: 'cjlpulido@hotmail.com'
+    },
+    {
+      id:1,
+      nombre: 'Andres',
+      apellidos: 'Garcia',
+      email: 'andres@hotmail.com'
+    },
+    {
+      id:2,
+      nombre: 'Ana',
+      apellidos: 'Hernandez',
+      email: 'anah@hotmail.com'
+    }
+  ]
+  
+  constructor () {}
 
 }
